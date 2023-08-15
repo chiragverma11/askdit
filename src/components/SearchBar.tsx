3a0eaacd5a3e7dfef0bfd9a51b5d0fe8cccb5310
Chirag Verma <chiragverma1013@gmail.com>
2023-08-15 23:35:36 +0530

    style: change the padding from div to input
    To create full SearchBar clickable

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,7 +8,7 @@ const SearchBar: FC<SearchBarProps> = ({ className, ...props }) => {
   return (
     <div
       className={cn(
-        "inline-flex w-4/6 items-center gap-2 rounded-lg border border-default/25 bg-emphasis px-6  py-2.5 text-sm text-subtle backdrop-blur-xl transition focus-within:border-default/90 focus-within:bg-subtle hover:border-default/90",
+        "inline-flex w-4/6 items-center gap-2 rounded-lg border border-default/25 bg-emphasis px-6 text-sm text-subtle backdrop-blur-xl transition focus-within:border-default/90 focus-within:bg-subtle hover:border-default/90",
         className,
       )}
       {...props}
@@ -16,7 +16,7 @@ const SearchBar: FC<SearchBarProps> = ({ className, ...props }) => {
       <Search className="h-5 w-5" />
       <input
         type="text"
-        className="w-full bg-transparent font-semibold placeholder:text-subtle focus-visible:outline-none"
+        className="w-full py-2.5 bg-transparent font-semibold placeholder:text-subtle focus-visible:outline-none"
         placeholder="Search"
       />
     </div>
