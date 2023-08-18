6b2d0e3654a1d6b46070f2daa3d9c1ce42b1319a
Chirag Verma <chiragverma1013@gmail.com>
2023-08-18 19:24:06 +0530

    style: fixes layout of sign-in/up pages

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -9,20 +9,18 @@ interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
   return (
-    <div className="absolute inset-0">
-      <div className="mx-auto flex h-full max-w-2xl flex-col items-center justify-center gap-10">
-        <Link
-          href={"/"}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "-mt-20 self-start hover:bg-zinc-200/75 dark:hover:bg-emphasis/50 dark:hover:ring dark:hover:ring-emphasis/60",
-          )}
-        >
-          <IoArrowBack className="mr-2 h-4 w-4" />
-          Home
-        </Link>
-        <SignUp />
-      </div>
+    <div className="mx-auto flex h-full max-w-2xl flex-col items-center gap-10 px-4 pt-4">
+      <Link
+        href={"/"}
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "self-start hover:bg-zinc-200/75 dark:hover:bg-emphasis/50 dark:hover:ring dark:hover:ring-emphasis/60",
+        )}
+      >
+        <IoArrowBack className="mr-2 h-4 w-4" />
+        Home
+      </Link>
+      <SignUp />
     </div>
   );
 };
