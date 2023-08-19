022836f76940cfb1046abadcd63e5a112ee823a7
Chirag Verma <chiragverma1013@gmail.com>
2023-08-19 16:46:01 +0530

    feat: secure routes with auth & middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,18 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
+  if (req.nextUrl.pathname.startsWith("/sign-in" || "sign-up")) {
+    if (token) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    return null;
+  }
+
   if (!token) {
     return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
   }
 }
 
 export const config = {
-  matcher: [],
+  matcher: ["/u/:path*", "/sign-in", "/sign-up"],
 };
