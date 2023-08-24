ddacee1e7f5a3468e92676b60208496bb5f07aa1
Chirag Verma <chiragverma1013@gmail.com>
2023-08-24 22:36:09 +0530

    feat: add community subscribe & unsubscribe

--- src/server/routers/community.ts
+++ src/server/routers/community.ts
@@ -41,4 +41,73 @@ export const communityRouter = router({
 
       return community.name;
     }),
+
+  //subscribe community
+  subscribe: protectedProcedure
+    .input(z.object({ communityId: z.string() }))
+    .mutation(async (opts) => {
+      const communityId = opts.input.communityId;
+      const { user } = opts.ctx;
+
+      const subscriptionExists = await db.subscription.findFirst({
+        where: {
+          subredditId: communityId,
+          userId: user.id,
+        },
+      });
+
+      if (subscriptionExists) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "You have already subscribed.",
+        });
+      }
+
+      await db.subscription.create({
+        data: {
+          subredditId: communityId,
+          userId: user.id,
+        },
+      });
+
+      return {
+        communityId,
+        message: "Subscribed successfully.",
+      };
+    }),
+  //subscribe community
+  unsubscribe: protectedProcedure
+    .input(z.object({ communityId: z.string() }))
+    .mutation(async (opts) => {
+      const communityId = opts.input.communityId;
+      const { user } = opts.ctx;
+
+      const subscriptionExists = await db.subscription.findFirst({
+        where: {
+          subredditId: communityId,
+          userId: user.id,
+        },
+      });
+
+      if (!subscriptionExists) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You've not been subscribed to this subreddit, yet.",
+        });
+      }
+
+      await db.subscription.delete({
+        where: {
+          userId_subredditId: {
+            userId: user.id,
+            subredditId: communityId,
+          },
+        },
+      });
+
+      return {
+        communityId,
+        message: "Unsubscribed successfully.",
+      };
+    }),
 });
