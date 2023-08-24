601b7b224f01b21e4600d40019f24067ddd5d4fd
Chirag Verma <chiragverma1013@gmail.com>
2023-08-24 22:29:04 +0530

    feat: use context and middleware in trpc with protectedProcedure
    To check user on every protectedProcedure request.

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,7 +1,22 @@
-import { initTRPC } from "@trpc/server";
+import { getAuthSession } from "@/lib/auth";
+import { TRPCError, initTRPC } from "@trpc/server";
+import { Context } from "./context";
 
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create();
 
 export const router = t.router;
 export const middleware = t.middleware;
+
+// to check if user is authenticated
+const isAuth = middleware(async (opts) => {
+  const session = await getAuthSession();
+
+  if (!session?.user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return opts.next({ ctx: { user: session.user } });
+});
+
 export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(isAuth);
