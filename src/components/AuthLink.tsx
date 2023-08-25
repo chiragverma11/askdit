dd1b60ca489e09334b5fcfc71b267a453a924e42
Chirag Verma <chiragverma1013@gmail.com>
2023-08-25 13:44:08 +0530

    feat: add paramsAsCallback prop to AuthLink

--- src/components/AuthLink.tsx
+++ src/components/AuthLink.tsx
@@ -1,24 +1,30 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { FC } from "react";
 
 interface AuthLinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
   href: string;
+  paramsAsCallback?: boolean;
 }
 
 const AuthLink: FC<AuthLinkProps> = ({
   href,
   className,
   children,
+  paramsAsCallback,
   ...props
 }) => {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const callbackUrl = paramsAsCallback
+    ? searchParams.get("callbackUrl") ?? "/"
+    : pathname;
 
   return (
     <Link
-      href={{ pathname: href, query: { callbackUrl: pathname } }}
+      href={{ pathname: href, query: { callbackUrl: callbackUrl } }}
       className={className}
       {...props}
     >
