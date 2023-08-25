55a03bd264b464a6a2704c89e9fab9007879d02f
Chirag Verma <chiragverma1013@gmail.com>
2023-08-25 22:38:19 +0530

    feat: add function to get bg color from community

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getDefaultCommunityBg({
+  communityName,
+}: {
+  communityName: string;
+}) {
+  const firstChar = communityName?.charAt(0);
+  return firstChar?.match(/[a-iA-I]/g)
+    ? "bg-amber-400"
+    : firstChar?.match(/[j-rJ-R]/g)
+    ? "bg-lime-400"
+    : firstChar?.match(/[s-zS-Z]/g)
+    ? "bg-violet-400"
+    : "";
+}
