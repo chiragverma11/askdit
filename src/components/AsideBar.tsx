c86422d1f01aa4946499763fd1cb47d018518f23
Chirag Verma <chiragverma1013@gmail.com>
2023-08-26 09:13:41 +0530

    refactor: replaces repetetive code to dynamic

--- src/components/AsideBar.tsx
+++ src/components/AsideBar.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { Flame, Home } from "lucide-react";
-import Link from "next/link";
-import { FC } from "react";
-import { HiOutlineUserGroup } from "react-icons/hi";
-import { LuEdit } from "react-icons/lu";
-import { Button, buttonVariants } from "./ui/Button";
 import {
   Tooltip,
   TooltipContent,
@@ -13,133 +7,81 @@ import {
   TooltipTrigger,
 } from "@/components/ui/Tooltip";
 import { cn } from "@/lib/utils";
+import { Flame, Home } from "lucide-react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FC } from "react";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import { LuEdit } from "react-icons/lu";
+import { buttonVariants } from "./ui/Button";
+
+const navItems = [
+  { path: "/", name: "Home", icon: "Home" },
+  { path: "/popular", name: "Popular", icon: "Flame" },
+  { path: "/answer", name: "Answer", icon: "LuEdit" },
+  { path: "/communities", name: "Communities", icon: "HiOutlineUserGroup" },
+];
+
+const Items = {
+  Home: "Home",
+  Popular: "Popular",
+  Answer: "Answer",
+  Communities: "Communities",
+} as const;
 
 interface AsideBarProps {}
 
 const AsideBar: FC<AsideBarProps> = ({}) => {
-  const pathname = usePathname();
+  const pathname = usePathname() || "/";
 
   return (
     <nav className="container fixed inset-x-0 bottom-0 z-10 flex h-[4.25rem] w-full items-center justify-center bg-subtle py-8 shadow-inner ring ring-zinc-300/50 backdrop-blur transition-colors dark:ring-0 sm:h-20 lg:inset-x-auto lg:bottom-4 lg:left-4 lg:top-16 lg:my-auto lg:h-[calc(100vh-15%)] lg:w-[10%] lg:rounded-3xl lg:bg-emphasis/80">
       <TooltipProvider skipDelayDuration={500}>
         <ul className="xl:gap flex h-full w-full items-center justify-between lg:flex-col lg:justify-evenly lg:gap-16 2xl:gap-20">
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href={"/"}
-                  className={cn(
-                    buttonVariants({ size: "icon" }),
-                    "w-14",
-                    pathname === "/"
-                      ? "hover:bg-primary/75"
-                      : "bg-transparent hover:bg-transparent lg:bg-subtle lg:hover:bg-default",
-                  )}
-                >
-                  <Home className="h-6 w-6 text-default" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent
-                side="bottom"
-                align="start"
-                alignOffset={25}
-                sideOffset={5}
-                className="hidden px-2 py-1 text-xs lg:block"
-              >
-                <p>Home</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href={"/"}
-                  className={cn(
-                    buttonVariants({ size: "icon" }),
-                    "w-14",
-                    pathname === "/popular"
-                      ? "hover:bg-primary/75"
-                      : "bg-transparent hover:bg-transparent lg:bg-subtle lg:hover:bg-default",
-                  )}
-                >
-                  <Flame className="h-6 w-6 text-default" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent
-                side="bottom"
-                align="start"
-                alignOffset={25}
-                sideOffset={5}
-                className="hidden px-2 py-1 text-xs lg:block"
-              >
-                <p>Popular</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href={"/"}
-                  className={cn(
-                    buttonVariants({ size: "icon" }),
-                    "w-14",
-                    pathname === "/answer"
-                      ? "hover:bg-primary/75"
-                      : "bg-transparent hover:bg-transparent lg:bg-subtle lg:hover:bg-default",
-                  )}
-                >
-                  <LuEdit className="h-6 w-6 text-default" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent
-                side="bottom"
-                align="start"
-                alignOffset={25}
-                sideOffset={5}
-                className="hidden px-2 py-1 text-xs lg:block"
-              >
-                <p>Answer</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
+          {navItems.map((item) => {
+            const isActive = item.path === pathname;
 
-          <li>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href={"/communities"}
-                  className={cn(
-                    buttonVariants({ size: "icon" }),
-                    "w-14",
-                    pathname === "/communities"
-                      ? "hover:bg-primary/75"
-                      : "bg-transparent hover:bg-transparent lg:bg-subtle lg:hover:bg-default",
-                  )}
-                >
-                  <HiOutlineUserGroup className="h-6 w-6 text-default" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent
-                side="bottom"
-                align="start"
-                alignOffset={25}
-                sideOffset={5}
-                className="hidden px-2 py-1 text-xs lg:block"
-              >
-                <p>Communities</p>
-              </TooltipContent>
-            </Tooltip>
-          </li>
-          {/* <li>
-          <Button size={"icon"} className="bg-transparent hover:bg-zinc-400/25">
-            <Link href={"/"}>
-              <LogOut className="h-6 w-6 text-zinc-500" />
-            </Link>
-          </Button>
-        </li> */}
+            const IconComponent =
+              item.name === Items.Home ? (
+                <Home className="h-6 w-6 text-default" />
+              ) : item.name === Items.Popular ? (
+                <Flame className="h-6 w-6 text-default" />
+              ) : item.name === Items.Answer ? (
+                <LuEdit className="h-6 w-6 text-default" />
+              ) : item.name === Items.Communities ? (
+                <HiOutlineUserGroup className="h-6 w-6 text-default" />
+              ) : null;
+
+            return (
+              <li key={item.path}>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={item.path}
+                      className={cn(
+                        buttonVariants({ size: "icon" }),
+                        "w-14",
+                        isActive
+                          ? "hover:bg-primary/75"
+                          : "bg-transparent hover:bg-transparent lg:bg-subtle lg:hover:bg-default",
+                      )}
+                    >
+                      {IconComponent}
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent
+                    side="bottom"
+                    align="start"
+                    alignOffset={25}
+                    sideOffset={5}
+                    className="hidden px-2 py-1 text-xs lg:block"
+                  >
+                    <p>{item.name}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </li>
+            );
+          })}
         </ul>
       </TooltipProvider>
     </nav>
