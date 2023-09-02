6e1e45916cc52b911a0e014f44d104635b070dc1
Chirag Verma <chiragverma1013@gmail.com>
2023-09-03 00:03:28 +0530

    feat: add Create Post page for community

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -1,9 +1,36 @@
+import AsideBar from "@/components/AsideBar";
+import Editor from "@/components/Editor";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
 import { FC } from "react";
 
-interface CreatePostSubredditProps {}
+interface CreatePostSubredditProps {
+  params: {
+    slug: string;
+  };
+}
 
-const CreatePostSubreddit: FC<CreatePostSubredditProps> = ({}) => {
-  return <div>CreatePostSubreddit</div>;
+const CreatePostSubreddit: FC<CreatePostSubredditProps> = async ({
+  params,
+}) => {
+  const community = await db.subreddit.findFirst({
+    where: { name: params.slug },
+  });
+
+  if (!community) return notFound();
+
+  return (
+    <>
+      <AsideBar />
+      <div className="flex w-full justify-center px-4 py-6 pt-4">
+        <div className="flex w-full flex-col lg:w-[680px]">
+          <h3 className="text-lg font-semibold">Create a Post</h3>
+          {/* <PostCommunitySelect /> */}
+          <Editor communityId={community?.id} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CreatePostSubreddit;
