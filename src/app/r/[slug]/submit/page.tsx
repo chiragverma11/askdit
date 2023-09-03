6051f4d554ee00d517c920503137d99fa02b2685
Chirag Verma <chiragverma1013@gmail.com>
2023-09-03 16:04:02 +0530

    feat: add dynamic metadata & getCommunity function

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -1,8 +1,18 @@
-import AsideBar from "@/components/AsideBar";
 import Editor from "@/components/Editor";
 import { db } from "@/lib/db";
+import { Metadata, ResolvingMetadata } from "next";
+import dynamic from "next/dynamic";
 import { notFound } from "next/navigation";
 import { FC } from "react";
+const AsideBar = dynamic(() => import("@/components/AsideBar"), { ssr: false });
+
+async function getCommunity({ name }: { name: string }) {
+  const community = await db.subreddit.findFirst({
+    where: { name },
+  });
+
+  return community;
+}
 
 interface CreatePostSubredditProps {
   params: {
@@ -10,12 +20,31 @@ interface CreatePostSubredditProps {
   };
 }
 
+export async function generateMetadata(
+  { params }: CreatePostSubredditProps,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const slug = params.slug;
+
+  const community = await getCommunity({ name: slug });
+
+  const previousTitle = (await parent).title || "";
+
+  if (!community) {
+    return {
+      title: previousTitle,
+    };
+  }
+
+  return {
+    title: `Submit to ${slug}`,
+  };
+}
+
 const CreatePostSubreddit: FC<CreatePostSubredditProps> = async ({
   params,
 }) => {
-  const community = await db.subreddit.findFirst({
-    where: { name: params.slug },
-  });
+  const community = await getCommunity({ name: params.slug });
 
   if (!community) return notFound();
 
@@ -25,7 +54,6 @@ const CreatePostSubreddit: FC<CreatePostSubredditProps> = async ({
       <div className="flex w-full justify-center px-4 py-6 pt-4">
         <div className="flex w-full flex-col lg:w-[680px]">
           <h3 className="text-lg font-semibold">Create a Post</h3>
-          {/* <PostCommunitySelect /> */}
           <Editor communityId={community?.id} />
         </div>
       </div>
