4ad1e4b439dd2c6b3ecc88917afad8d8b9a01781
Chirag Verma <chiragverma1013@gmail.com>
2023-09-07 07:40:03 +0530

    feat(trpc): add postRouter to appRouter of trpc

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc";
 import { communityRouter } from "./community";
+import { postRouter } from "./post";
 
 export const appRouter = router({
   community: communityRouter,
+  post: postRouter,
 });
 
 export type AppRouter = typeof appRouter;
