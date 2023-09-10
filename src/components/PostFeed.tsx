112c0426cf6832d6c2e5414aa296e653da9d164c
Chirag Verma <chiragverma1013@gmail.com>
2023-09-10 08:35:32 +0530

    feat: add Post component to PostFeed

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,15 +1,35 @@
+"use client";
+
+import { getVotesAmount } from "@/lib/utils";
+import { Comment, Subreddit, User, Vote } from "@prisma/client";
 import { FC } from "react";
 import Post from "./Post";
 
-interface PostFeedProps {}
+interface PostFeedProps {
+  initialPosts: (Post & {
+    author: User;
+    votes: Vote[];
+    subreddit: Subreddit;
+    comments: Comment[];
+  })[];
+  communityName?: string;
+}
 
-const PostFeed: FC<PostFeedProps> = ({}) => {
+const PostFeed: FC<PostFeedProps> = ({ initialPosts, communityName }) => {
   return (
     <div className="space-y-3 pb-16 lg:pb-0">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {initialPosts.map((post) => {
+        const votesAmt = getVotesAmount({ votes: post.votes });
+
+        return (
+          <Post
+            post={post}
+            votesAmt={votesAmt}
+            key={post.id}
+            isCommunity={communityName ? true : false}
+          />
+        );
+      })}
     </div>
   );
 };
