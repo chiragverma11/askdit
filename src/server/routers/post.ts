fde5ebbe9f6a3394721ad9cd11222b9ded7f8306
Chirag Verma <chiragverma1013@gmail.com>
2023-09-12 08:08:14 +0530

    feat(trpc): add votePost protected procedure

--- src/server/routers/post.ts
+++ src/server/routers/post.ts
@@ -1,4 +1,4 @@
-import { PostValidator } from "@/lib/validators/post";
+import { PostValidator, PostVoteValidator } from "@/lib/validators/post";
 import { protectedProcedure, router } from "../trpc";
 import { db } from "@/lib/db";
 import { TRPCError } from "@trpc/server";
@@ -34,5 +34,67 @@ export const postRouter = router({
       });
 
       return { postId: post.id, message: "Post created successfully" };
+    }),
+  votePost: protectedProcedure
+    .input(PostVoteValidator)
+    .mutation(async (opts) => {
+      const { postId, voteType } = opts.input;
+      const { user } = opts.ctx;
+
+      const existingVote = await db.vote.findFirst({
+        where: {
+          userId: user.id,
+          postId,
+        },
+      });
+
+      const post = await db.post.findUnique({
+        where: { id: postId },
+        include: { author: true, votes: true },
+      });
+
+      if (!post) {
+        return new Response("Post not found", { status: 404 });
+      }
+
+      if (existingVote) {
+        if (existingVote.type === voteType) {
+          await db.vote.delete({
+            where: { userId_postId: { postId, userId: user.id } },
+          });
+
+          // const votesAmt = post.votes.reduce((acc, vote) => {
+          //   if (vote.type === "UP") return acc + 1;
+          //   if (vote.type === "DOWN") return acc - 1;
+          //   return acc;
+          // }, 0);
+
+          return new Response("OK");
+        }
+
+        await db.vote.update({
+          where: {
+            userId_postId: {
+              postId,
+              userId: user.id,
+            },
+          },
+          data: {
+            type: voteType,
+          },
+        });
+
+        return new Response("OK");
+      }
+
+      await db.vote.create({
+        data: {
+          type: voteType,
+          userId: user.id,
+          postId,
+        },
+      });
+
+      return new Response("OK");
     }),
 });
