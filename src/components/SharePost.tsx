4b826156ae55cc8e681f728e28c8ac661f9f6319
Chirag Verma <chiragverma1013@gmail.com>
2023-09-14 22:22:23 +0530

    🩹 fix(drawer): fix overflow hidden on body when open
    To fix hidden header on drawer open by making modal false & adding
    custom css on openChange function to body

--- src/components/SharePost.tsx
+++ src/components/SharePost.tsx
@@ -63,7 +63,20 @@ const SharePostDrawer = ({ postUrl, onCopyLink, post }: SharePostMenuProps) => {
   const [open, setOpen] = useState(false);
 
   return (
-    <Drawer.Root open={open} onOpenChange={setOpen}>
+    <Drawer.Root
+      open={open}
+      onOpenChange={(open) => {
+        setOpen(open);
+        if (open === true) {
+          document.body.style.pointerEvents = "none";
+          document.body.style.overflow = "hidden";
+        } else {
+          document.body.style.removeProperty("overflow");
+          document.body.style.removeProperty("pointer-events");
+        }
+      }}
+      modal={false}
+    >
       <Drawer.Trigger asChild>
         <span className="z-[1] inline-flex cursor-pointer items-center gap-1 rounded-3xl bg-subtle px-3 py-2 text-zinc-400 hover:bg-highlight/60">
           <TbShare3 className="h-5 w-5" />
@@ -71,45 +84,46 @@ const SharePostDrawer = ({ postUrl, onCopyLink, post }: SharePostMenuProps) => {
         </span>
       </Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className="fixed inset-0 z-10 bg-zinc-700/30" />
-        <Drawer.Content className="fixed bottom-0 left-0 right-0 z-10 mt-24 flex flex-col rounded-t-2xl bg-emphasis dark:bg-default">
-          <div className="flex-1 rounded-t-[10px] p-4">
-            <div className="mx-auto mb-6 h-1.5 w-12 flex-shrink-0 rounded-full bg-highlight" />
-            <div className="flex flex-col gap-5 pb-4 text-sm font-semibold text-default">
-              <button
-                className="inline-flex items-center"
-                onClick={() => setOpen(false)}
-              >
-                <BookmarkPlus className="mr-2 h-4 w-4" strokeWidth={2.125} />
-                Bookmark
-              </button>
-              <button
-                onClick={() => {
-                  setOpen(false);
-                  onCopyLink();
-                }}
-                className="inline-flex items-center"
-              >
-                <LinkIcon className="mr-2 h-4 w-4" strokeWidth={2.125} />
-                Copy Link
-              </button>
-              <div>
-                <RWebShare
-                  data={{
-                    title: post.title,
-                    url: postUrl.toString(),
-                  }}
+        <div className="fixed inset-0 z-10 bg-zinc-700/40">
+          <Drawer.Content className="fixed bottom-0 left-0 right-0 z-10 mt-24 flex flex-col rounded-t-2xl bg-emphasis dark:bg-default">
+            <div className="flex-1 rounded-t-[10px] p-4">
+              <div className="mx-auto mb-6 h-1.5 w-12 flex-shrink-0 rounded-full bg-highlight" />
+              <div className="flex flex-col gap-5 pb-4 text-sm font-semibold text-default">
+                <button
+                  className="inline-flex items-center"
                   onClick={() => setOpen(false)}
                 >
-                  <button className="inline-flex items-center">
-                    <Share2 className="mr-2 h-4 w-4" strokeWidth={2.125} />
-                    Share post via...
-                  </button>
-                </RWebShare>
+                  <BookmarkPlus className="mr-2 h-4 w-4" strokeWidth={2.125} />
+                  Bookmark
+                </button>
+                <button
+                  onClick={() => {
+                    setOpen(false);
+                    onCopyLink();
+                  }}
+                  className="inline-flex items-center"
+                >
+                  <LinkIcon className="mr-2 h-4 w-4" strokeWidth={2.125} />
+                  Copy Link
+                </button>
+                <div>
+                  <RWebShare
+                    data={{
+                      title: post.title,
+                      url: postUrl.toString(),
+                    }}
+                    onClick={() => setOpen(false)}
+                  >
+                    <button className="inline-flex items-center">
+                      <Share2 className="mr-2 h-4 w-4" strokeWidth={2.125} />
+                      Share post via...
+                    </button>
+                  </RWebShare>
+                </div>
               </div>
             </div>
-          </div>
-        </Drawer.Content>
+          </Drawer.Content>
+        </div>
       </Drawer.Portal>
     </Drawer.Root>
   );
