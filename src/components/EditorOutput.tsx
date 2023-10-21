ba90fa77356f389d05be5e037b5d06fe7b81c34a
Chirag Verma <chiragverma1013@gmail.com>
2023-10-21 21:40:03 +0530

    ✨ feat: add custom renderer & style to editor output

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,17 +1,42 @@
+"use client";
+
 import dynamic from "next/dynamic";
 import { FC } from "react";
-// const Output = dynamic(
-//   async () => (await import("editorjs-react-renderer")).default,
-//   { ssr: false },
-// );
-import Output from "editorjs-react-renderer";
+import CustomImageRenderer from "./renderers/CustomImageRenderer";
+const Output = dynamic(
+  async () => (await import("editorjs-react-renderer")).default,
+  { ssr: false },
+);
+
+const renderers = {
+  image: CustomImageRenderer,
+};
+
+const style = {
+  paragraph: {
+    fontSize: "0.875rem",
+    lineHeight: "1.25rem",
+  },
+  linkTool: {
+    color: "red",
+    borderRadius: "1rem",
+  },
+};
 
 interface EditorOutputProps {
   content: any;
 }
 
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
-  return <Output data={content} className="text-sm" />;
+  return (
+    <Output
+      style={style}
+      className="text-sm"
+      renderers={renderers}
+      data={content}
+      key="output"
+    />
+  );
 };
 
 export default EditorOutput;
