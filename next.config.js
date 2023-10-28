4d4a410df4affea2291a53cb78faceb894094bf2
Chirag Verma <chiragverma1013@gmail.com>
2023-10-28 17:46:34 +0530

    🔧 fix(next): update depreceated `domains` with `remotePatterns` for images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "utfs.io"],
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "utfs.io" },
+    ],
   },
 };
 
