c48cb6e57c2063160dcd6d94ec237b14a858d26d
Chirag Verma <chiragverma1013@gmail.com>
2023-10-30 19:23:44 +0530

    ♻️ refactor: change title range to 1-300 characters

--- src/lib/validators/post.ts
+++ src/lib/validators/post.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 export const PostValidator = z.object({
   title: z
     .string()
-    .min(3, {
-      message: "Title must be at least 3 characters long",
+    .min(1, {
+      message: "Title must be at least 1 character long",
     })
-    .max(128, {
+    .max(300, {
       message: "Title must be less than 300 characters long",
     }),
   content: z.any(),
