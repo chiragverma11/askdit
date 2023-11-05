c2a76c8ae177c0838fdcb72479c046a483d911c3
Chirag Verma <chiragverma1013@gmail.com>
2023-11-05 13:53:48 +0530

    ♿️ feat: add border on tab focus-within & negative tabIndex to inputs to CreatePostInput

--- src/components/CreatePostInput.tsx
+++ src/components/CreatePostInput.tsx
@@ -35,15 +35,16 @@ const CreatePostInput: FC<CreatePostInputProps> = ({
 
         <div
           className={cn(
-            "inline-flex w-full items-center gap-2 rounded-lg bg-subtle px-6 text-sm text-subtle backdrop-blur-xl focus-within:border-default/90 focus-within:bg-subtle/60 focus-within:transition-colors dark:focus-within:bg-default",
+            "inline-flex w-full items-center gap-2 rounded-lg bg-subtle px-6 text-sm text-subtle backdrop-blur-xl focus-within:border focus-within:border-default/90 focus-within:bg-subtle/60 focus-within:transition-colors dark:focus-within:bg-default",
           )}
         >
           {href ? (
-            <Link href={`${href}`} className="w-full">
+            <Link href={`${href}`} className="w-full focus:outline-none">
               <input
                 type="text"
                 className="w-full bg-transparent py-2.5 font-semibold placeholder:text-subtle focus-visible:outline-none"
                 placeholder="Create Post"
+                tabIndex={-1}
               />
             </Link>
           ) : (
@@ -51,6 +52,7 @@ const CreatePostInput: FC<CreatePostInputProps> = ({
               type="text"
               className="w-full bg-transparent py-2.5 font-semibold placeholder:text-subtle focus-visible:outline-none"
               placeholder="Create Post"
+              tabIndex={-1}
             />
           )}
         </div>
