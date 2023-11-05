51c1f153db662c8b055d54048583440961d27f29
Chirag Verma <chiragverma1013@gmail.com>
2023-11-05 18:52:05 +0530

    ✨ feat(api): add check for og-image accessibility

--- src/app/api/link/route.ts
+++ src/app/api/link/route.ts
@@ -13,6 +13,32 @@ export async function GET(req: NextRequest) {
 
     const metadata = await urlMetadata(href);
 
+    try {
+      const controller = new AbortController();
+      const id = setTimeout(() => controller.abort(), 5000);
+
+      const imageResponse = await fetch(metadata["og:image"] as string, {
+        signal: controller.signal,
+      });
+
+      clearTimeout(id);
+
+      if (!imageResponse.ok) {
+        throw new Error("Image not accessible");
+      }
+    } catch (error) {
+      return new Response(
+        JSON.stringify({
+          success: 1,
+          meta: {
+            title: metadata.title,
+            description: metadata.description,
+            image: { url: undefined },
+          },
+        }),
+      );
+    }
+
     return new Response(
       JSON.stringify({
         success: 1,
