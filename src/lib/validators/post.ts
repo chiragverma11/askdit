da073e6f74c2c075e3d2d17f9c62b40ce088ed8e
Chirag Verma <chiragverma1013@gmail.com>
2023-11-06 19:33:59 +0530

    ✨ feat: add LinkPost to show link type post output

--- src/lib/validators/post.ts
+++ src/lib/validators/post.ts
@@ -16,6 +16,24 @@ export const PostValidator = z.object({
   type: z.nativeEnum(PostType),
 });
 
+export const PostLinkValidator = z.object({
+  title: z
+    .string()
+    .min(1, {
+      message: "Title must be at least 1 character long",
+    })
+    .max(POST_TITLE_LENGTH, {
+      message: `Title must be less than ${POST_TITLE_LENGTH} characters long`,
+    }),
+  content: z.object({
+    url: z.string().url(),
+    domain: z.string().min(1),
+    ogImage: z.string().url().min(1).optional(),
+  }),
+  communityId: z.string(),
+  type: z.nativeEnum(PostType),
+});
+
 export const PostVoteValidator = z.object({
   postId: z.string(),
   voteType: z.enum(["UP", "DOWN"]),
