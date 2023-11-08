b6fee96d26cd8f56e97333bfe17e532ff41638c7
Chirag Verma <chiragverma1013@gmail.com>
2023-11-08 17:38:22 +0530

    ✨ feat: add description in InfoSideMenu

--- src/components/InfoSideMenu.tsx
+++ src/components/InfoSideMenu.tsx
@@ -1,11 +1,12 @@
-import { Session } from "next-auth";
-import { FC } from "react";
-import { Separator } from "./ui/Separator";
-import SubscribeLeaveToggle from "./SubscribeLeaveToggle";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "./ui/Button";
 import type { getCommunity } from "@/lib/prismaQueries";
+import { cn } from "@/lib/utils";
+import { Session } from "next-auth";
+import Link from "next/link";
+import { FC } from "react";
+import CommunityDescription from "./CommunityDescription";
+import SubscribeLeaveToggle from "./SubscribeLeaveToggle";
+import { buttonVariants } from "./ui/Button";
+import { Separator } from "./ui/Separator";
 
 interface InfoSideMenuProps {
   isSubscribed: boolean;
@@ -20,6 +21,8 @@ const InfoSideMenu: FC<InfoSideMenuProps> = ({
 }) => {
   if (!community) return null;
 
+  const isAuthor = session?.user.id === community.creatorId;
+
   return (
     <div className="absolute right-[-2rem] top-0 hidden justify-self-end xl:block">
       <div className="fixed flex flex-col gap-4 rounded-xl border border-default/40 bg-emphasis px-4 py-4">
@@ -32,19 +35,22 @@ const InfoSideMenu: FC<InfoSideMenuProps> = ({
               subredditName={community?.name}
               session={session}
               className="hidden lg:inline-flex"
-              disabled={session?.user.id === community?.creatorId}
+              disabled={isAuthor}
             />
           </div>
-          <div className="flex w-full flex-col items-center gap-4">
+          <div className="flex h-auto w-full flex-col items-center gap-4 text-sm">
+            <CommunityDescription
+              initialDescription={community.description}
+              communityId={community.id}
+              isAuthor={isAuthor}
+            />
             <div className="flex w-full items-center justify-between">
-              <p className="text-sm">Members</p>
-              <p className="text-sm">{community._count.subscribers}</p>
+              <p className="text-default/40">Members</p>
+              <p>{community._count.subscribers}</p>
             </div>
             <div className="flex w-full items-center justify-between">
-              <p className="text-sm">Created</p>
-              <p className="text-sm">
-                {new Date(community.createdAt).toDateString()}
-              </p>
+              <p className="text-default/40">Created</p>
+              <p>{new Date(community.createdAt).toDateString()}</p>
             </div>
             <Separator />
             <Link
