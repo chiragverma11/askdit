740077dcc6a963f63846e84c86c1e8a9276f9e74
Chirag Verma <chiragverma1013@gmail.com>
2023-11-26 08:13:44 +0530

    🩹 fix: make authparameters route dynamic & authentication protected

--- src/app/api/imagekit/authparameters/route.ts
+++ src/app/api/imagekit/authparameters/route.ts
@@ -1,8 +1,19 @@
 import { env } from "@/env.mjs";
+import { getAuthSession } from "@/lib/auth";
 import ImageKit from "imagekit";
+
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
+    }
+
     const imagekit = new ImageKit({
       publicKey: env.IMAGEKIT_PUBLIC_KEY,
       privateKey: env.IMAGEKIT_PRIVATE_KEY,
