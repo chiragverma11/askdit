fc192cbb1e996aaefbfb57b614d08a5001215166
Chirag Verma <chiragverma1013@gmail.com>
2023-11-28 23:30:47 +0530

    ✨ feat(home): add no-subscription & no-post fallback to AuthenticatedFeed

--- src/components/homepage/AuthenticatedFeed.tsx
+++ src/components/homepage/AuthenticatedFeed.tsx
@@ -1,7 +1,10 @@
 import { getAuthenticatedFeedPosts } from "@/lib/prismaQueries";
+import { cn } from "@/lib/utils";
 import { Session } from "next-auth";
+import Link from "next/link";
 import { FC } from "react";
 import PostFeed from "../PostFeed";
+import { buttonVariants } from "../ui/Button";
 
 interface AuthenticatedFeedProps {
   session: Session | null;
@@ -12,12 +15,33 @@ const AuthenticatedFeed: FC<AuthenticatedFeedProps> = async ({ session }) => {
     userId: session?.user.id,
   });
 
+  if (subscriptions.length === 0 && posts.length === 0) {
+    return <NoAuthenticatedPost />;
+  }
+
   return (
     <PostFeed
       initialPosts={posts}
       session={session}
       communityIds={subscriptions.map((sub) => sub.subredditId)}
     />
+  );
+};
+
+const NoAuthenticatedPost = () => {
+  return (
+    <div className="flex flex-col items-center gap-4 p-4">
+      <p className="text-default">You haven&apos;t joined any community yet</p>
+      <Link
+        href="/communities?explore=true"
+        className={cn(
+          buttonVariants({ size: "sm", variant: "outline" }),
+          "rounded-lg",
+        )}
+      >
+        Explore Communities
+      </Link>
+    </div>
   );
 };
 
