fc31ab941135bfde638d89ef636971e7578afd8c
Chirag Verma <chiragverma1013@gmail.com>
2023-11-28 20:53:58 +0530

    🎨 refactor(communities): modifies YourCommunities  ExploreCommunities with CommunityCard

--- src/components/communities/ExploreCommunities.tsx
+++ src/components/communities/ExploreCommunities.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 import { trpc } from "@/lib/trpc";
-import { cn, getDefaultCommunityBg } from "@/lib/utils";
-import { motion } from "framer-motion";
 import { Session } from "next-auth";
-import Link from "next/link";
 import { FC } from "react";
-import "react-loading-skeleton/dist/skeleton.css";
 import CommunitiesFallback from "./CommunitiesFallback";
+import CommunityCard from "./CommunityCard";
 
 interface ExploreCommunitiesProps {
   session: Session | null;
@@ -22,64 +18,13 @@ const ExploreCommunities: FC<ExploreCommunitiesProps> = ({ session }) => {
     return <CommunitiesFallback type="nocontent" parent="ExploreCommunities" />;
   }
 
-  return (
-    <>
-      {!isLoading
-        ? exploreCommunities?.map((community) => {
-            const defaultProfileBg = getDefaultCommunityBg({
-              communityName: community?.name,
-            });
+  if (isLoading) {
+    return <CommunitiesFallback type="loading" />;
+  }
 
-            return (
-              <motion.div
-                key={community.id}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                className="w-full rounded-xl border border-default/20 bg-emphasis px-4 py-4"
-              >
-                <Link
-                  href={`/r/${community.name}`}
-                  className="flex w-full items-center justify-between"
-                >
-                  <div className="flex items-center gap-3">
-                    <span
-                      className={cn(
-                        "flex aspect-square h-10 w-10 items-center justify-center rounded-full text-2xl font-bold text-zinc-950",
-                        defaultProfileBg,
-                      )}
-                    >
-                      r/
-                    </span>
-                    <div className="flex flex-col gap-2">
-                      <h1 className="text-lg font-semibold md:text-xl">
-                        r/{community.name}
-                      </h1>
-                      <p className="text-xs text-subtle">
-                        {community._count.subscribers}{" "}
-                        {community._count.subscribers > 1
-                          ? "members"
-                          : "member"}
-                      </p>
-                    </div>
-                  </div>
-                  <SubscribeLeaveToggle
-                    isSubscribed={false}
-                    subredditId={community?.id}
-                    subredditName={community?.name}
-                    session={session}
-                    disableRefresh={true}
-                    disabled={session?.user.id === community?.creatorId}
-                  />
-                </Link>
-              </motion.div>
-            );
-          })
-        : Array.from({ length: 4 }, () => "").map((elem, index) => (
-            <CommunitiesFallback type="loading" key={index} />
-          ))}
-    </>
-  );
+  return exploreCommunities?.map((community) => (
+    <CommunityCard key={community.id} community={community} session={session} />
+  ));
 };
 
 export default ExploreCommunities;
--- src/components/communities/YourCommunities.tsx
+++ src/components/communities/YourCommunities.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 import { trpc } from "@/lib/trpc";
-import { cn, getDefaultCommunityBg } from "@/lib/utils";
-import { motion } from "framer-motion";
 import { Session } from "next-auth";
-import Link from "next/link";
 import { FC } from "react";
-import "react-loading-skeleton/dist/skeleton.css";
 import CommunitiesFallback from "./CommunitiesFallback";
+import CommunityCard from "./CommunityCard";
 
 interface YourCommunitiesProps {
   session: Session | null;
@@ -22,66 +18,17 @@ const YourCommunities: FC<YourCommunitiesProps> = ({ session }) => {
     return <CommunitiesFallback type="nocontent" parent="YourCommunities" />;
   }
 
-  return (
-    <>
-      {!isLoading
-        ? yourCommunities?.map((community) => {
-            const defaultProfileBg = getDefaultCommunityBg({
-              communityName: community?.Subreddit.name,
-            });
+  if (isLoading) {
+    return <CommunitiesFallback type="loading" />;
+  }
 
-            return (
-              <motion.div
-                key={community.subredditId}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                className="w-full rounded-xl border border-default/20 bg-emphasis px-4 py-4"
-              >
-                <Link
-                  href={`/r/${community.Subreddit.name}`}
-                  className="flex w-full items-center justify-between"
-                >
-                  <div className="flex items-center gap-3">
-                    <span
-                      className={cn(
-                        "flex aspect-square h-10 w-10 items-center justify-center rounded-full text-2xl font-bold text-zinc-950",
-                        defaultProfileBg,
-                      )}
-                    >
-                      r/
-                    </span>
-                    <div className="flex flex-col gap-2">
-                      <h1 className="text-lg font-semibold md:text-xl">
-                        r/{community.Subreddit.name}
-                      </h1>
-                      <p className="text-xs text-subtle">
-                        {community.Subreddit._count.subscribers}{" "}
-                        {community.Subreddit._count.subscribers > 1
-                          ? "members"
-                          : "member"}
-                      </p>
-                    </div>
-                  </div>
-                  <SubscribeLeaveToggle
-                    isSubscribed={true}
-                    subredditId={community?.subredditId}
-                    subredditName={community?.Subreddit.name}
-                    session={session}
-                    disableRefresh={true}
-                    disabled={
-                      session?.user.id === community?.Subreddit.creatorId
-                    }
-                  />
-                </Link>
-              </motion.div>
-            );
-          })
-        : Array.from({ length: 4 }, () => "").map((elem, index) => (
-            <CommunitiesFallback type="loading" key={index} />
-          ))}
-    </>
-  );
+  return yourCommunities?.map((community) => (
+    <CommunityCard
+      key={community.subredditId}
+      community={community.Subreddit}
+      session={session}
+    />
+  ));
 };
 
 export default YourCommunities;
