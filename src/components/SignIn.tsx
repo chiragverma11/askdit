b3d0ffd35d43f570a72c459e8952ea2f71032e3b
Chirag Verma <chiragverma1013@gmail.com>
2023-11-29 18:29:46 +0530

    🎨 refactor: modifies sign-in & sign-up modal to improve structure of code & renames

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -7,7 +7,7 @@ interface SignInProps {}
 
 const SignIn: FC<SignInProps> = ({}) => {
   return (
-    <div className="container mx-auto flex flex-col justify-center gap-10">
+    <div className="flex w-full flex-col justify-center gap-10">
       <div className="flex flex-col items-center gap-6">
         <div className="flex h-16 w-16 items-center justify-center rounded-full bg-gradient-radial from-brand-default/20 to-subtle/20 drop-shadow backdrop-blur">
           <Icons.logo className="h-10 w-10" />
