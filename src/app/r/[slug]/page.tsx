c0487c205249ab486805106ca77c30b420fcc1e2
Chirag Verma <chiragverma1013@gmail.com>
2023-12-06 16:24:52 +0530

    ♻️ refactor: modifies CommunityHeader to show different content for different screen sizes

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,19 +1,17 @@
 import CommunityInfoCard from "@/components/CommunityInfoCard";
 import PostFeed from "@/components/PostFeed";
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import UserAvatar from "@/components/UserAvatar";
 import FeedWrapper from "@/components/layout/FeedWrapper";
 import MainContentWrapper from "@/components/layout/MainContentWrapper";
 import SideMenuWrapper from "@/components/layout/SideMenuWrapper";
-import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { getCommunity, getSubscription } from "@/lib/prismaQueries";
 import { cn, getDefaultCommunityBg } from "@/lib/utils";
 import { Metadata } from "next";
 import { Session } from "next-auth";
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import { FC } from "react";
-import { MdOutlinePostAdd } from "react-icons/md";
 
 interface SubredditPageProps {
   params: {
@@ -99,48 +97,48 @@ const CommunityHeader = ({
   });
 
   return (
-    <div className="mb-3 flex w-full flex-col justify-between gap-4 lg:mb-4 lg:flex-row">
-      <div className="flex items-center justify-between px-4">
+    <div className="mb-3 flex w-full flex-col justify-between gap-4 px-4 lg:mb-4 lg:flex-row lg:items-center">
+      <div className="flex w-full items-center justify-between">
         <div className="flex items-center gap-3">
+          {community.image ? (
+            <UserAvatar
+              className="h-12 w-12"
+              user={{ name: community.name, image: community.image }}
+            />
+          ) : (
           <span
             className={cn(
-              "flex aspect-square h-10 w-10 items-center justify-center rounded-full text-2xl font-bold text-zinc-950",
+                "flex aspect-square h-12 w-12 items-center justify-center rounded-full text-2xl font-bold text-zinc-950",
               defaultProfileBg,
             )}
           >
             r/
           </span>
+          )}
           <div className="flex flex-col">
-            <h1 className="text-lg font-semibold md:text-xl">
+            <h1 className="text-lg font-bold lg:text-2xl">
               r/{community.name}
             </h1>
+
             <p className="text-xs text-subtle lg:hidden">
               {community._count.subscribers}{" "}
               {community._count.subscribers > 1 ? "members" : "member"}
             </p>
           </div>
         </div>
+        <div className="flex justify-center">
         <SubscribeLeaveToggle
           isSubscribed={isSubscribed}
           subredditId={community?.id}
           subredditName={community?.name}
           session={session}
-          className="lg:hidden"
+            className="w-20 rounded-full"
           disabled={session?.user.id === community?.creatorId}
         />
+        </div>
       </div>
-
-      {session?.user ? (
-        <Link
-          href={`/r/${community.name}/submit`}
-          className={cn(
-            buttonVariants({ variant: "ghost", size: "sm" }),
-            "flex w-fit items-center rounded-3xl border-2 border-transparent font-semibold hover:border-default/50 hover:bg-transparent",
-          )}
-        >
-          <MdOutlinePostAdd className="mr-2 h-6 w-6" />
-          Create a Post
-        </Link>
+      {community.description ? (
+        <div className="texts text-sm lg:hidden">{community.description}</div>
       ) : null}
     </div>
   );
