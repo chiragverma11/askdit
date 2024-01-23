3ffdb5b29858b3d33e9b1287ada0b243cc28044f
Chirag Verma <chiragverma1013@gmail.com>
2024-01-23 21:05:13 +0530

    💄 style: modify some styles in FeedFilterOptions for mobile devices

--- src/components/FeedFilterOptions.tsx
+++ src/components/FeedFilterOptions.tsx
@@ -1,11 +1,17 @@
+import { cn } from "@/lib/utils";
 import { FC } from "react";
 import FeedViewTypeSelector from "./FeedViewTypeSelector";
 
-interface FeedFilterOptionProps {}
+interface FeedFilterOptionProps extends React.ComponentPropsWithoutRef<"div"> {}
 
-const FeedFilterOptions: FC<FeedFilterOptionProps> = ({}) => {
+const FeedFilterOptions: FC<FeedFilterOptionProps> = ({ className }) => {
   return (
-    <div className="mb-3 flex gap-4 border-b border-default/40 px-4 py-2 md:rounded-xl md:border md:bg-emphasis">
+    <div
+      className={cn(
+        "mb-3 flex gap-4 border-y border-default/25 bg-emphasis px-4 py-2 sm:mb-4 md:rounded-xl md:border",
+        className,
+      )}
+    >
       <FeedViewTypeSelector className="h-8 border-0 bg-transparent data-[state=open]:bg-emphasis md:bg-emphasis md:data-[state=open]:bg-subtle" />
     </div>
   );
