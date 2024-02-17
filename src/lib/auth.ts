32178afbf2698e5d1fd2d44ee1e2b7ff00b44b8a
Chirag Verma <chiragverma1013@gmail.com>
2024-02-17 21:10:31 +0530

    ♻️ refactor(auth): replace findFirst with findUnique for user

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -24,17 +24,24 @@ export const authOptions: NextAuthOptions = {
       if (token) {
         session.user.id = token.id;
         session.user.name = token.name;
+        session.user.username = token.username;
         session.user.email = token.email;
         session.user.image = token.picture;
-        session.user.username = token.username;
       }
 
       return session;
     },
     async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
+      const dbUser = await db.user.findUnique({
         where: {
-          email: token.email,
+          id: token.id,
+        },
+        select: {
+          id: true,
+          name: true,
+          username: true,
+          email: true,
+          image: true,
         },
       });
 
@@ -59,9 +66,9 @@ export const authOptions: NextAuthOptions = {
       return {
         id: dbUser.id,
         name: dbUser.name,
+        username: dbUser.username,
         email: dbUser.email,
         picture: dbUser.image,
-        username: dbUser.username,
       };
     },
   },
