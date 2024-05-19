a63abf80626001d3ce8996cca7827924cdcd88c1
Chirag Verma <chiragverma1013@gmail.com>
2024-05-19 19:06:44 +0530

    ✨ feat: add NoContent fallback to answer & popular feeds

--- src/components/answer/AnswerFeed.tsx
+++ src/components/answer/AnswerFeed.tsx
@@ -1,5 +1,11 @@
 import { getQuestions } from "@/lib/prismaQueries";
 import { FC } from "react";
+import {
+  NoContent,
+  NoContentAction,
+  NoContentDescription,
+  NoContentTitle,
+} from "../NoContent";
 import PostFeed from "../PostFeed";
 
 interface AnswerFeedProps {
@@ -10,6 +16,20 @@ const AnswerFeed: FC<AnswerFeedProps> = async ({ currentUserId }) => {
   const { communityIds, posts: questions } = await getQuestions({
     currentUserId,
   });
+
+  if (questions.length === 0) {
+    return (
+      <NoContent>
+        <NoContentTitle>There are no questions to answer</NoContentTitle>
+        <NoContentDescription>
+          Join a communtiy to ask or answer questions
+        </NoContentDescription>
+        <NoContentAction href="/communities?explore=true">
+          Explore Communities
+        </NoContentAction>
+      </NoContent>
+    );
+  }
 
   return (
     <PostFeed
--- src/components/popular/PopularFeed.tsx
+++ src/components/popular/PopularFeed.tsx
@@ -1,6 +1,12 @@
-import { FC } from "react";
-import PostFeed from "../PostFeed";
 import { getPopularPosts } from "@/lib/prismaQueries";
+import { FC } from "react";
+import {
+  NoContent,
+  NoContentAction,
+  NoContentDescription,
+  NoContentTitle,
+} from "../NoContent";
+import PostFeed from "../PostFeed";
 
 interface PopularFeedProps {
   currentUserId: string | undefined;
@@ -8,6 +14,20 @@ interface PopularFeedProps {
 
 const PopularFeed: FC<PopularFeedProps> = async ({ currentUserId }) => {
   const popularPosts = await getPopularPosts({ currentUserId });
+
+  if (popularPosts.length === 0) {
+    return (
+      <NoContent>
+        <NoContentTitle>
+          hmm... looks like there are no posts yet
+        </NoContentTitle>
+        <NoContentDescription>
+          Create one and get this feed started
+        </NoContentDescription>
+        <NoContentAction href="/submit">Create Post</NoContentAction>
+      </NoContent>
+    );
+  }
 
   return (
     <PostFeed
