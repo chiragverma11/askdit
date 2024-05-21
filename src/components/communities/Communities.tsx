365dd11420a6b920208b3342fdf5dab827c3e762
Chirag Verma <chiragverma1013@gmail.com>
2024-05-21 19:52:40 +0530

    ✨ feat: replace horizontal buttons or selector with HorizontalMenu

--- src/components/communities/Communities.tsx
+++ src/components/communities/Communities.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { useAutoAnimate } from "@formkit/auto-animate/react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { Session } from "next-auth";
 import { FC, useState } from "react";
 import "react-loading-skeleton/dist/skeleton.css";
+import HorizontalMenu from "../HorizontalMenu";
 import { ScrollArea } from "../ui/Scroll-Area";
 import CommunitiesFallback from "./CommunitiesFallback";
 import ExploreCommunities from "./ExploreCommunities";
 import YourCommunities from "./YourCommunities";
+
+const communitiesMenus = ["Your Communities", "Explore"] as const;
 
 interface CommunitiesProps {
   session: Session | null;
@@ -23,38 +26,17 @@ const Communities: FC<CommunitiesProps> = ({ session, explore }) => {
 
   return (
     <div className="flex flex-col gap-5">
-      <div className="flex items-center gap-6 px-4 lg:gap-10">
-        {["Your Communities", "Explore"].map((menuName) => {
-          const isActive = menuName === activeMenu;
-          const menu = menuName as typeof activeMenu;
-
-          return (
-            <button
-              className="relative cursor-pointer text-xl font-semibold text-default lg:text-2xl"
-              key={menuName}
-              onClick={() => {
-                setActiveMenu(menu);
-              }}
-            >
-              {menuName}
-              {isActive ? (
-                <motion.div
-                  className="absolute inset-x-0 -bottom-2 -z-10 h-1 rounded-md bg-primary"
-                  layoutId="communitiesMenu"
-                  aria-hidden="true"
-                  transition={{
-                    type: "spring",
-                    bounce: 0.01,
-                    stiffness: 140,
-                    damping: 18,
-                    duration: 0.3,
-                  }}
-                />
-              ) : null}
-            </button>
-          );
-        })}
-      </div>
+      <HorizontalMenu
+        menuId="_communitiesMenu"
+        items={communitiesMenus.map((menuName) => ({
+          name: menuName,
+          href: `${menuName === "Explore" ? "/communities?explore=true" : "/communities"}`,
+          className: "relative text-xl font-semibold text-default lg:text-2xl",
+          onClick: () => setActiveMenu(menuName),
+        }))}
+        isActive={(item) => item.name === activeMenu}
+        listContainerProps={{ className: "gap-6 pb-0 lg:gap-10" }}
+      />
       <ScrollArea className="h-[calc(var(--mobile-height-adjusted)-68px)] lg:h-[calc(var(--height-adjusted)-68px)]">
         <div
           className="flex w-full flex-col items-center gap-4 px-4 pb-16 lg:max-h-none lg:pb-4 lg:pr-4"
--- src/components/user/UserFeedSelector.tsx
+++ src/components/user/UserFeedSelector.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import { useMounted } from "@/hooks/use-mounted";
-import { cn } from "@/lib/utils";
-import { useIntersection } from "@mantine/hooks";
-import { motion } from "framer-motion";
-import Link from "next/link";
 import { useSelectedLayoutSegments } from "next/navigation";
-import { ElementRef, FC, useMemo, useRef } from "react";
-import { Icons } from "../Icons";
-import { Button } from "../ui/Button";
+import { FC } from "react";
+import HorizontalMenu from "../HorizontalMenu";
 
 const userProfileMenus = [
   "Posts",
@@ -50,124 +44,16 @@ const UserFeedSelector: FC<UserFeedSelectorProps> = ({
     ? userProfileMenus
     : userProfileMenus.filter((menu) => !privateMenus.includes(menu));
 
-  const selectorContainerRef = useRef<ElementRef<"ul">>(null);
-
-  const { ref: firstItemRef, entry: firstItemEntry } = useIntersection({
-    root: selectorContainerRef.current,
-    threshold: 1,
-  });
-  const { ref: lastItemRef, entry: lastItemEntry } = useIntersection({
-    root: selectorContainerRef.current,
-    threshold: 1,
-  });
-
-  const isMounted = useMounted();
-
-  const canScrollPrev = useMemo(
-    () => isMounted && !firstItemEntry?.isIntersecting,
-    [firstItemEntry?.isIntersecting, isMounted],
-  );
-  const canScrollNext = useMemo(
-    () => isMounted && !lastItemEntry?.isIntersecting,
-    [isMounted, lastItemEntry?.isIntersecting],
-  );
-
   return (
-    <div className="relative mb-4">
-      <Button
-        onClick={() => {
-          if (canScrollPrev) {
-            selectorContainerRef.current?.scrollTo({
-              left: 0,
-              behavior: "smooth",
-            });
-          }
-        }}
-        className={cn(
-          "absolute left-0 z-10 my-auto flex aspect-square h-full w-auto items-center justify-center rounded-full bg-transparent hover:bg-highlight/60",
-          canScrollPrev ? "" : "hidden",
-        )}
-        variant={"secondary"}
-        size={"icon"}
-        disabled={!canScrollPrev}
-      >
-        <Icons.chevronLeft className="w1/2 h-1/2" />
-        <span className="sr-only">Scroll left</span>
-      </Button>
-      <Button
-        onClick={() => {
-          if (canScrollNext) {
-            selectorContainerRef.current?.scrollTo({
-              left: selectorContainerRef.current?.scrollWidth,
-              behavior: "smooth",
-            });
-          }
-        }}
-        className={cn(
-          "absolute right-0 z-10 flex aspect-square h-full w-auto items-center justify-center rounded-full bg-transparent hover:bg-highlight/60",
-          canScrollNext ? "" : "hidden",
-        )}
-        variant={"secondary"}
-        size={"icon"}
-        disabled={!canScrollNext}
-      >
-        <Icons.chevronRight className="h-1/2 w-1/2" />
-        <span className="sr-only">Scroll left</span>
-      </Button>
-      <ul
-        ref={selectorContainerRef}
-        className={cn(
-          "no-scrollbar flex h-fit max-w-[100vw] shrink items-center gap-4 overflow-x-scroll px-4 pb-1 sm:overflow-x-auto lg:gap-8",
-          canScrollPrev && canScrollNext
-            ? "gradient-mask-r-[transparent,transparent_5%,rgba(0,0,0,1.0)_64px,rgba(0,0,0,1.0)_90%,transparent]"
-            : !canScrollPrev && canScrollNext
-              ? "gradient-mask-r-[black,rgba(0,0,0,1.0)_64px,rgba(0,0,0,1.0)_90%,transparent]"
-              : canScrollPrev && !canScrollNext
-                ? "gradient-mask-r-[transparent,transparent_5%,rgba(0,0,0,1.0)_64px,rgba(0,0,0,1.0)_100%]"
-                : "",
-        )}
-      >
-        {menus.map((menuName, index) => {
-          const isActive = menuName === activeMenu;
-
-          return (
-            <li
-              ref={
-                index === 0
-                  ? firstItemRef
-                  : index === menus.length - 1
-                    ? lastItemRef
-                    : null
-              }
-              key={menuName}
-            >
-              <Link
-                href={`/u/${username}/${
-                  menuName !== "Posts" ? menuName.toLowerCase() : ""
-                }`}
-                className="relative font-semibold text-default lg:text-lg"
-              >
-                {menuName}
-                {isActive ? (
-                  <motion.div
-                    className="absolute inset-x-0 -bottom-1.5 -z-10 h-1 rounded-md bg-primary"
-                    layoutId="userProfileMenu"
-                    aria-hidden="true"
-                    transition={{
-                      type: "spring",
-                      bounce: 0.01,
-                      stiffness: 140,
-                      damping: 18,
-                      duration: 0.3,
-                    }}
-                  />
-                ) : null}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <HorizontalMenu
+      menuId="userFeedSelector"
+      items={menus.map((menu) => ({
+        name: menu,
+        href: `/u/${username}/${menu !== "Posts" ? menu.toLowerCase() : ""}`,
+      }))}
+      isActive={(item) => item.name === activeMenu}
+      className="mb-4"
+    />
   );
 };
 
