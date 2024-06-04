4b28a1e018fc8270702ccaf31fd42c993596e542
Chirag Verma <chiragverma1013@gmail.com>
2024-06-04 10:27:59 +0530

    ✨ feat: update back button in sign-in and sign-up page

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,10 +1,7 @@
+import BackButton from "@/components/BackButton";
 import SignUp from "@/components/SignUp";
 import MainContentWrapper from "@/components/layout/MainContentWrapper";
-import { buttonVariants } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
 import { FC } from "react";
-import { IoArrowBack } from "react-icons/io5";
 
 interface SignUpPageProps {}
 
@@ -12,16 +9,10 @@ const SignUpPage: FC<SignUpPageProps> = ({}) => {
   return (
     <MainContentWrapper>
       <div className="flex h-full max-w-2xl grow flex-col items-center gap-10 px-4 pt-4">
-        <Link
-          href={"/"}
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "self-start hover:bg-zinc-200/75 dark:hover:bg-emphasis/50 dark:hover:ring dark:hover:ring-emphasis/60",
-          )}
-        >
-          <IoArrowBack className="mr-2 h-4 w-4" />
-          Home
-        </Link>
+        <BackButton className="flex aspect-auto h-auto w-auto items-center gap-1 self-start p-2 font-medium">
+          Back
+        </BackButton>
+
         <SignUp />
       </div>
     </MainContentWrapper>
