61519d41b7be72621acb6fa0c61c256b7f7e2f01
Chirag Verma <chiragverma1013@gmail.com>
2024-06-17 08:48:05 +0530

    🔥 refactor: remove unwanted code & files

--- src/app/r/[slug]/not-found.tsx
+++ src/app/r/[slug]/not-found.tsx
@@ -8,7 +8,6 @@ import FeedWrapper from "@/components/layout/FeedWrapper";
 import MainContentWrapper from "@/components/layout/MainContentWrapper";
 import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
 
 export default function NotFound() {
   return (
@@ -39,32 +38,5 @@ export default function NotFound() {
         </NoContent>
       </FeedWrapper>
     </MainContentWrapper>
-  );
-  return (
-    <div className="flex h-[90vh] w-full items-center justify-center">
-      <div className="flex flex-col justify-center gap-4 text-center">
-        <h2 className="text-lg">
-          Sorry, there aren’t any communities on Askdit with that name.
-        </h2>
-        <p className="text-sm">
-          This community may have been banned or the community name is
-          incorrect.
-        </p>
-        <div className="mx-auto mt-4 flex gap-4">
-          <Link
-            href="/communities/create"
-            className={cn(
-              buttonVariants({ variant: "outline", size: "sm" }),
-              "bg-emphasis hover:bg-subtle",
-            )}
-          >
-            Create Community
-          </Link>
-          <Link href="/" className={cn(buttonVariants({ size: "sm" }))}>
-            Go Home
-          </Link>
-        </div>
-      </div>
-    </div>
   );
 }
